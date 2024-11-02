de2ad31911f96928d91d7915c27c24e58147d70f
matias22lucena <matias22lucena@gmail.com>
2024-11-02 16:00:45 -0300

    Conexion a Sql

---
+++ rubi/server/server.js
@@ -0,0 +1,41 @@
+// server.js
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// Configuración de la conexión MySQL
+const db = mysql.createConnection({
+  host: 'localhost',
+  port: '3306',      
+  user: 'root',      
+  password: 'matute', 
+  database: 'sistema_de_venta' 
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error al conectar a MySQL:', err);
+    return;
+  }
+  console.log('Conectado a MySQL');
+});
+
+app.get('/api/usuarios', (req, res) => {
+  db.query('SELECT * FROM usuarios', (err, results) => {
+    if (err) {
+      res.status(500).send('Error en la consulta');
+      return;
+    }
+    res.json(results);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
--- rubi/src/componentes/home.jsx
+++ rubi/src/componentes/home.jsx
@@ -1,7 +1,25 @@
-export function Home(){
+/*
+export function Home({ user }){
     return (
         <div>
             <h1>Bienvenido</h1>
+            <h2>{user}</h2>
         </div>
     )
+}
+    */
+import PropTypes from 'prop-types'  // Importa PropTypes
+
+export function Home({ user }) {   // Asegúrate de recibir `user` como prop
+    return (
+        <div>
+            <h1>Bienvenido</h1>
+            <p>Usuario: {user[0]}</p>
+        </div>
+    )
+}
+
+// Validación de PropTypes para user
+Home.propTypes = {
+    user: PropTypes.arrayOf(PropTypes.string).isRequired
 }
